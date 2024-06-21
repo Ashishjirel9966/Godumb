bd72d7874bbaba6346bb2fef7d0f0bbf85e3b6b4
Ashishjirel9966 <97277277+Ashishjirel9966@users.noreply.github.com>
2024-06-21 19:34:35 +0545

    third commit

--- src/Pages/main.jsx
+++ src/Pages/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
+
+  React.useEffect(() => {
+    if (sessionStorage.getItem('loggedIn') === 'false') {
+      navigate('/login');
+    }
+  }, [navigate]);
   return (
     <div>
         <h1>Hell yeah</h1>
